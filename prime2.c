78d2d8ff81d2b427ae11b0ebd48523a8e49f5dcc
Yakshita <101899906+Yakshita1802@users.noreply.github.com>
2022-10-10 11:25:11 -0400

    Add files via upload

---
+++ prime2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+int get_valid_number();
+_Bool is_prime(int x);  //without <stdboo.h>, boolean is defined by _Bool
+
+//main() uses a sentinel controlled loop to test the primality of numbers
+//, without knowing how many numbers to be tested.
+// -1 is the sentinel value
+// the loop repeats the cycle of read->test->process->read ...
+
+//This code contains several bugs, please fix them so that
+//the prime test works correctly and produces the results 
+//as in the sample output
+
+//Please make your edits as least invasive as possible.
+//The working submission which makes the least edits will receive full points
+//and the other working submission will be evaluated based on how close it
+//is to the best submission
+ 
+int main() {
+	int n;
+        n = get_valid_number(); //read
+	while (n!=-1) { //test
+		//process
+		if (is_prime(n)) {
+			printf("%d is a prime number!\n", n);
+		}
+		else {
+			printf("%d is not a prime number!\n", n);
+		}
+		//int n;  -- this int n doen't required as int n is already defined in int (main)
+		//read again then loop back to test
+		n = get_valid_number();
+	}
+	return 0;
+}
+
+_Bool is_prime(int n) {
+	//if n is divisible by any number greater or equal to 2 and less than n, then n is not prime.
+	for (int i = 2; i < n; i++) {
+		if (n%i == 0) {
+			return 0;      //without boolean library, false and true needed to be as 0 and 1 resp.
+		}
+	}
+	return 1;
+}
+
+int get_valid_number(void) {
+	//The valid number is either -1 (to exit the program) or an integer greater than 2.
+	//A loop is used to implement input validation, that is, user will be asked to 
+	//enter a valid number over and over again until a valid number is received.
+        int n;
+	printf("Please enter an integer greater than 2 to test its primality, enter -1 to exit \n");
+	scanf("%d", &n);
+	while (n <= 2 && n != -1) { //&& condition is used, as both <=2 and !=-1 condition need to be satisfied so while loop will return n, and then condition for prime can be check for n
+		printf("Please enter an integer greater than 2 to test its primality, enter -1 to exit \n");
+		scanf("%d", &n);
+	}
+	return n;
+}
